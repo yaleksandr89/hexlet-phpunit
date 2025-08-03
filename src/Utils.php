54ad5270cf5c4e1bc81a375fac63d8e9c2585499
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2025-08-04 00:04:59 +0300

    Добавлен Utils->without и UtilsTest->without

--- src/Utils.php
+++ src/Utils.php
@@ -7,3 +7,13 @@ function reverseString($string)
 {
     return implode(array_reverse(str_split($string)));
 }
+
+function without(array $coll, array $values = []): array
+{
+    if (empty($values)) {
+        return $coll;
+    }
+    // array_diff сравнивает значения и возвращает только те, что есть в первом, но нет во втором
+    // array_values возвращает новый индексированный массив (чтобы индексы были с 0)
+    return array_values(array_diff($coll, $values));
+}
--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -3,7 +3,9 @@
 namespace Hexlet\Phpunit\Tests;
 
 use PHPUnit\Framework\TestCase;
+
 use function Hexlet\Phpunit\Utils\reverseString;
+use function Hexlet\Phpunit\Utils\without;
 
 // Класс UtilsTest наследует класс TestCase
 // Имя класса совпадает с именем файла
@@ -18,5 +20,14 @@ class UtilsTest extends TestCase
         // И только потом актуальное (actual)
         $this->assertEquals('', reverseString(''));
         $this->assertEquals('olleh', reverseString('hello'));
+    }
+
+    public function testWithout(): void
+    {
+        $this->assertEquals([], without([], [1, 2, 3, 4, 5]));
+        $this->assertEquals([1, 2, 3, 4, 5], without([1, 2, 3, 4, 5], []));
+        $this->assertEquals([1, 2, 3], without([1, 2, 3, 4, 5], [4, 5]));
+        $this->assertEquals([1, 1], without([1, 2, 2, 1], [2]));
+        $this->assertEquals([], without([2, 2, 2, 2], [2]));
     }
 }
